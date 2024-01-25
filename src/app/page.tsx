7a67b4bde42f2d8dfca845f37260bf5c1c185b2d
tatazzi <otaviol.gon@gmail.com>
2024-01-25 19:58:13 -0300

    refactor: update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,34 +2,30 @@
 
 import styles from "./page.module.css";
 import Logo from "../assets/images/Logo.png";
+import HomeSvg from "../assets/Svgs/home.svg";
 import Image from "next/image";
-import Button from "@/components/Button";
-import HomeIcon from "../assets/Svgs/home.svg";
-import NavigationOptions from "@/components/NavigationOptions";
 import ProductBoard from "@/components/ProductBoard";
-import NewProductModal from "@/components/NewProductModal";
-import { useState } from "react";
 
 export default function Home() {
-  const [isVisible, setIsVisible] = useState(false);
-
   return (
     <main className={styles.container}>
       <div className={styles["nav-bar"]}>
         <Image width={150} height={150} alt="Logo" src={Logo} />
+        <div className={styles["button-wrapper"]}>
+          <Image
+            className={styles["add-icon"]}
+            alt="button icon"
+            src={HomeSvg}
+            width={50}
+            height={50}
+          />
+          <p className={styles.paragraph}>Home</p>
+        </div>
       </div>
       <div className={styles.hero}>
-        <div className={styles["aside-nav"]}>
-          <NavigationOptions icon={HomeIcon} label="Inicio" />
-        </div>
         <div className={styles["product-display"]}>
-          <Button onClick={() => setIsVisible(true)}>Novo</Button>
           <ProductBoard />
         </div>
-        <NewProductModal
-          isVisible={isVisible}
-          onClose={() => setIsVisible(false)}
-        />
       </div>
     </main>
   );
